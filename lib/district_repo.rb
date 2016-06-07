712632c2208750e969b758ddfb6270f24b19f261
Kris Sparks <kbs5280@gmail.com>
2016-06-07 14:09:55 -0600

    First tests and methods working

--- lib/district_repo.rb
+++ lib/district_repo.rb
@@ -1,7 +1,26 @@
-class DistrictRepo
+require 'csv'
 
-def initialize
-disctricts = {}
-end
+class DistrictRepo
+  attr_reader :districts
+
+  def initialize
+    @districts = []
+  end
+
+  def load_data(data)
+    file = data.dig(:enrollment, :kindergarten)
+    contents = CSV.open file, headers: true, header_converters: :symbol
+      contents.each do |row|
+      districts << row[:location]
+    end
+  end
+
+  def find_by_name(district)
+    districts.detect { |district| district }
+    #go into the hash containing the file
+    #search the file for the district
+    #return the distric object
+    binding.pry
+  end
 
 end
--- test/district_repo_test.rb
+++ test/district_repo_test.rb
@@ -1,1 +1,38 @@
-require './lib/test_helper'
+require './test/test_helper'
+require './lib/district_repo'
+
+class DistrictRepoTest < Minitest::Test
+
+  def test_it_loads_data_by_length
+    dr = DistrictRepo.new
+    dr.load_data({
+        :enrollment => {
+          :kindergarten => "./data/Kindergartners in full-day program.csv"
+          }
+        })
+    assert_equal 181, dr.districts.uniq.length
+  end
+
+  def test_it_loads_data_by_checking_first
+    dr = DistrictRepo.new
+    dr.load_data({
+        :enrollment => {
+          :kindergarten => "./data/Kindergartners in full-day program.csv"
+          }
+        })
+    assert_equal "Colorado", dr.districts.uniq.first
+  end
+
+  def test_it_can_find_by_name
+    skip
+    dr = DistrictRepo.new
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"
+        }
+      })
+      district = dr.find_by_name("ACADEMY 20")
+      assert_equal "<District>", district
+  end
+
+end
