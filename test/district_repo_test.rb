8d7d40347f1586b482d5e764400430367e7d8528
Kris Sparks <kbs5280@gmail.com>
2016-06-07 18:09:58 -0600

    Iteration 0 complete

--- test/district_repo_test.rb
+++ test/district_repo_test.rb
@@ -5,14 +5,14 @@ require './lib/district'
 class DistrictRepoTest < Minitest::Test
 
   def test_it_loads_data_by_length
-    skip
     dr = DistrictRepo.new
     dr.load_data({
         :enrollment => {
           :kindergarten => "./data/Kindergartners in full-day program.csv"
           }
         })
-    assert_equal 181, dr.districts.uniq.length
+    district = dr.find_by_name("ACADEMY 20")
+    assert_instance_of District, district
   end
 
   def test_it_can_find_by_name
@@ -20,8 +20,8 @@ class DistrictRepoTest < Minitest::Test
     d2 = District.new({:name => "ACADEMY 20"})
     dr = DistrictRepo.new([d1, d2])
 
-    assert_equal "Adams", dr.find_by_name("Adams").attributes[:name]
-    assert_equal "ACADEMY 20", dr.find_by_name("ACADEMY 20").attributes[:name]
+    assert_equal d1, dr.find_by_name("Adams")
+    assert_equal d2, dr.find_by_name("ACADEMY 20")
   end
 
   def test_find_by_name_returns_nil
@@ -32,5 +32,12 @@ class DistrictRepoTest < Minitest::Test
     assert_equal nil, dr.find_by_name("Not_a_name")
   end
 
+  def test_find_by_name_by_fragment
+    d1 = District.new({:name => "Adams"})
+    d2 = District.new({:name => "ACADEMY 20"})
+    dr = DistrictRepo.new([d1, d2])
+
+    assert_equal [d1, d2], dr.find_all_matching("A")
+  end
 
 end
--- test/enrollment_repo_test.rb
+++ test/enrollment_repo_test.rb
@@ -2,7 +2,6 @@ require './test/test_helper'
 require './lib/enrollment_repo'
 require './lib/enrollment'
 
-
 class EnrollmentRepoTest < Minitest::Test
 
   def test_it_can_load_data_and_find_name
@@ -21,8 +20,8 @@ class EnrollmentRepoTest < Minitest::Test
     e2 = Enrollment.new({:name => "ACADEMY 20"})
     er = EnrollmentRepo.new([e1, e2])
 
-    assert_equal "Adams", er.find_by_name("Adams").attributes[:name]
-    assert_equal "ACADEMY 20", er.find_by_name("ACADEMY 20").attributes[:name]
+    assert_equal e1, er.find_by_name("Adams")
+    assert_equal e2, er.find_by_name("ACADEMY 20")
   end
 
   def test_find_by_name_returns_nil
