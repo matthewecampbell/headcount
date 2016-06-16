66d5a3d030e5893e18c5cf54a4a3d7eff8cfad76
Matthew Campbell <ematthewcampbell@gmail.com>
2016-06-16 08:48:13 -0600

    finished

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -42,12 +42,29 @@ class EnrollmentRepositoryTest < Minitest::Test
   end
 
   def test_enrollments_holds_enrollment_data
-    e1 = Enrollment.new({:name => "Adams"})
-    e2 = Enrollment.new({:name => "WOODLAND PARK RE-2"})
-    er = EnrollmentRepository.new({"Adams" => e1, "WOODLAND PARK RE-2" => e2})
+    er = EnrollmentRepository.new
+    er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv"
+      }
+    })
+    e1 = er.find_by_name("ACADEMY 20")
+    e2 = er.find_by_name("WOODLAND PARK RE-2")
 
-    name = er.find_by_name("WOODLAND PARK RE-2")
-    assert_equal ({:name=>"WOODLAND PARK RE-2"}), name.attributes
+    
+    assert_equal ([:name, :kindergarten_participation, :high_school_graduation]), e1.attributes.keys
+    assert_equal "ACADEMY 20", e1.attributes.values.first
+    assert_equal ({2010=>0.895, 2011=>0.895, 2012=>0.88983, 2013=>0.91373, 2014=>0.898}), e1.attributes.values.last
+    assert_instance_of Hash, e1.attributes
+    assert_equal 3, e1.attributes.count
+
+    assert_equal ([:name, :kindergarten_participation, :high_school_graduation]), e2.attributes.keys
+    assert_equal "WOODLAND PARK RE-2", e2.attributes.values.first
+    assert_equal ({2010=>0.814, 2011=>0.852, 2012=>0.85837, 2013=>0.79911, 2014=>0.755}), e2.attributes.values.last
+    assert_instance_of Hash, e2.attributes
+    assert_equal 3, e2.attributes.count
+
   end
 
 end
